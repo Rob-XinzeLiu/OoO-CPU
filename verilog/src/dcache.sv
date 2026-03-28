afa35ddc904762f705b1b5ded699edcc3ce4cbbf
Carlos A Garcia Lopez <cargarci@caen-vnc-mi09.engin.umich.edu>
2026-03-28 15:44:56 -0400

    dcache and mshr interface added not finished integration with lsq

---
+++ verilog/src/dcache.sv
@@ -0,0 +1,405 @@
+`include "sys_defs.svh"
+`include "ISA.svh"
+
+module Dcache 
+#(
+    parameter int WAYS = 4,
+    parameter int SETS = 8,
+    parameter int LINES = SETS * WAYS,
+    parameter int LINE_BYTES = 8
+)(
+    input   logic            clock,
+    input   logic            reset,
+    input   INST             inst,
+    input   ADDR             request_address,
+    input   logic            req_is_load, 
+    input   DATA             req_store_data,
+    input   logic            req_valid,
+    input   completed_mshr_t com_miss_req, // from mshr
+    input   logic            miss_returned,
+
+    output  dcache_data_t   cache_resp_data, // slot 2 will be for cache miss loads
+    output  logic            resp_valid,
+    output  logic            cache_ready, // it is ready while the miss queue is not full
+    output  miss_request_t   miss_request,
+
+    // write buffer interface (VC dirty evictions)
+    output  vc_wb_req_t      wb_req,  //to write buffer
+    input   logic            wb_ready //from write buffer
+);
+
+    localparam int ADDR_BITS   = $bits(ADDR);
+    localparam int OFFSET_BITS = $clog2(LINE_BYTES);
+    localparam int SET_BITS  = $clog2(SETS);
+    localparam int TAG_BITS    = ADDR_BITS - OFFSET_BITS - SET_BITS;
+
+    MEM_SIZE d_request_size;
+    logic                    d_req_unsigned;
+
+    miss_request_t next_miss_request;
+
+    cache_tag_t cache_tags [SETS-1:0][WAYS-1:0];
+    cache_tag_t next_cache_tags [SETS-1:0][WAYS-1:0];
+
+    logic [TAG_BITS-1:0]     d_request_tag;
+    logic [SET_BITS-1:0]     d_request_set;
+    logic [OFFSET_BITS-1:0]  d_request_offset;
+    logic                    hit;
+    logic [$clog2(WAYS)-1:0] hit_way;
+
+    //logic [$clog2(WAYS)-1:0] lru_way; //to tell VC we're eveicting 
+
+
+    //////////////////////
+    // MEMDP VARIABLES //
+    //////////////////////
+
+    localparam int DATA_DEPTH = SETS * WAYS;
+    logic                           data_re; // read enable per way
+    logic [$clog2(DATA_DEPTH)-1:0]  data_raddr; //read addr per way
+    MEM_BLOCK                       data_rdata; 
+
+    logic                           data_we; // write enable per way
+    logic [$clog2(DATA_DEPTH)-1:0]  data_waddr; // write addr per way
+    MEM_BLOCK                       data_wdata; // write data per way
+    
+    assign d_request_size   = MEM_SIZE'(inst.r.funct3[1:0]);
+    assign d_req_unsigned   = inst.r.funct3[2];
+
+    assign d_request_tag    = request_address[ADDR_BITS-1 : OFFSET_BITS + SET_BITS];
+    assign d_request_set    = request_address[OFFSET_BITS + SET_BITS - 1 : OFFSET_BITS];
+    assign d_request_offset = request_address[OFFSET_BITS-1:0];
+
+    assign cache_ready  = !miss_returned ; // && mshr is not full // TODO: PLEASE ENSURE THAT THIS CHANGES. for now cache ready
+
+
+   
+    // VC signals
+    logic                    vc_hit;
+    MEM_BLOCK                vc_hit_data;
+    logic                    vc_hit_dirty;
+    logic                    vc_evicted_ready;
+    logic                    vc_lookup_valid;
+
+    logic                    dcache_evicted_valid;
+    logic [`DCACHE_TAG_BITS-1:0] dcache_evicted_tag;
+    logic [`DCACHE_SET_BITS-1:0] dcache_evicted_set;
+    MEM_BLOCK                dcache_evicted_data;
+    logic                    dcache_evicted_dirty;
+ 
+    //LRU LOGIC STUFF
+    logic [$clog2(WAYS)-1:0] old_lru_index_hit;
+
+    logic                    found_way_miss;
+    logic [$clog2(WAYS)-1:0] way_index_miss;
+    logic [$clog2(WAYS)-1:0] old_lru_index_miss;
+    
+    assign vc_lookup_valid = req_valid && !hit;
+
+    logic is_valid_load;
+    logic is_valid_store;
+
+    assign is_valid_load = req_valid && req_is_load;
+    assign is_valid_store = req_valid && !req_is_load;
+
+    MEM_BLOCK selected_read_data;
+    MEM_BLOCK merged_store_data;
+
+    assign selected_read_data = hit ? data_rdata : '0 ;
+
+
+    function automatic logic [$clog2(SETS*WAYS)-1:0] flat_idx(
+        input logic [SET_BITS-1:0] set_idx,
+        input logic [$clog2(WAYS)-1:0] way_idx
+    );
+        flat_idx = set_idx * WAYS + way_idx;
+    endfunction
+
+    always_comb begin 
+        next_cache_tags   = cache_tags;
+
+        next_miss_request = '0;
+
+        hit     = 1'b0;
+        hit_way = '0;
+        
+       
+        data_re    = 1'b0;
+        data_raddr = '0;
+        data_we    = 1'b0;
+        data_waddr = '0;
+        data_wdata = '0;
+    
+
+        old_lru_index_hit = '0;
+
+        dcache_evicted_valid = 1'b0;
+        dcache_evicted_tag   = '0;
+        dcache_evicted_set   = '0;
+        dcache_evicted_data  = '0;
+        dcache_evicted_dirty = '0;
+
+        cache_resp_data = '0;
+        resp_valid      = 1'b0;
+
+        //data_re    = req_valid;
+        /////////////////
+        // Miss logic  //
+        /////////////////
+
+        found_way_miss      = '0;
+        way_index_miss      = '0;
+        old_lru_index_miss  = '0;
+
+        if (miss_returned) begin
+            // STEP 1. CHECK IF THERE IS AN INVALID WAY IN THE SET.
+            for (int i = 0; i < WAYS; ++i) begin
+                if (!cache_tags[com_miss_req.miss_req_set][i].valid && !found_way_miss) begin
+                    found_way_miss = 1'b1;
+                    way_index_miss = [$clog2(WAYS)-1:0](i); // cast this
+                end 
+                else if (cache_tags[com_miss_req.miss_req_set][i].lru_val == 'd0 && !found_way_miss) begin
+                    way_index_miss = [$clog2(WAYS)-1:0](i); // cast this
+                    found_way_miss = 1'b1;
+                end
+            end
+
+            if (found_way_miss) begin
+                // If overwriting a valid dcache line, send it to VC
+                
+                data_we = 1'b1;
+                data_waddr = flat_idx(com_miss_req.miss_req_set, way_index_miss); // is this the correct address if we are filling up an entire 64 bit block
+                data_wdata = com_miss_req.refill_data;
+
+                if (com_miss_req.req_is_load) begin
+                    cache_resp_data.address = com_miss_req.miss_req_address;
+                    cache_resp_data.valid   = 1'b1;
+
+                    unique case (com_miss_req.miss_req_size)
+                        BYTE: begin
+                            cache_resp_data.data = com_miss_req.miss_req_unsigned ?
+                                {{24{1'b0}}, com_miss_req.refill_data.byte_level[com_miss_req.miss_req_offset]} :
+                                {{24{com_miss_req.refill_data.byte_level[com_miss_req.miss_req_offset][7]}},
+                                com_miss_req.refill_data.byte_level[com_miss_req.miss_req_offset]};
+                        end
+
+                        HALF: begin
+                            cache_resp_data.data = com_miss_req.miss_req_unsigned ?
+                                {{16{1'b0}}, com_miss_req.refill_data.half_level[com_miss_req.miss_req_offset[2:1]]} :
+                                {{16{com_miss_req.refill_data.half_level[com_miss_req.miss_req_offset[2:1]][15]}},
+                                com_miss_req.refill_data.half_level[com_miss_req.miss_req_offset[2:1]]};
+                        end
+
+                        WORD: begin
+                            cache_resp_data.data =
+                                com_miss_req.refill_data.word_level[com_miss_req.miss_req_offset[2]];
+                        end
+
+                        default: begin
+                            cache_resp_data.data = '0;
+                        end
+                    endcase
+
+                    resp_valid = 1'b1;
+                end
+
+                if(!com_miss_req.req_is_load) begin
+                    merged_store_data = com_miss_req.refill_data;
+                    data_we = 1'b1;
+                    data_waddr = flat_idx(com_miss_req.miss_req_set, way_index_miss);
+                    case (com_miss_req.miss_req_size)
+                        BYTE: merged_store_data.byte_level[com_miss_req.miss_req_offset] = com_miss_req.miss_req_data[7:0];
+                        HALF: merged_store_data.half_level[com_miss_req.miss_req_offset[2:1]] = com_miss_req.miss_req_data[15:0];
+                        WORD: merged_store_data.word_level[com_miss_req.miss_req_offset[2]] = com_miss_req.miss_req_data;
+                    endcase
+
+                    data_wdata = merged_store_data;
+                    next_cache_tags[com_miss_req.miss_req_set][way_index_miss].dirty = 1'b1;
+                end
+    
+                if (cache_tags[com_miss_req.miss_req_set][way_index_miss].valid) begin
+                    data_re = 1'b1;
+                    data_raddr = flat_idx(com_miss_req.miss_req_set, way_index_miss);
+
+                    dcache_evicted_valid = 1'b1;
+                    dcache_evicted_tag   = cache_tags[com_miss_req.miss_req_set][way_index_miss].tag;
+                    dcache_evicted_set   = com_miss_req.miss_req_set;
+                    dcache_evicted_data  = data_rdata; 
+                    dcache_evicted_dirty = cache_tags[com_miss_req.miss_req_set][way_index_miss].dirty;
+                end
+
+                if (!dcache_evicted_valid || vc_evicted_ready) begin  // update lru logic here
+                    old_lru_index_miss =
+                        cache_tags[com_miss_req.miss_req_set][way_index_miss].valid ?
+                        cache_tags[com_miss_req.miss_req_set][way_index_miss].lru_val : 'd0;
+
+                    next_cache_tags[com_miss_req.miss_req_set][way_index_miss].tag   = com_miss_req.miss_req_tag;
+                    next_cache_tags[com_miss_req.miss_req_set][way_index_miss].valid = 1'b1;
+                    next_cache_tags[com_miss_req.miss_req_set][way_index_miss].dirty = !com_miss_req.req_is_load;
+                    next_cache_tags[com_miss_req.miss_req_set][way_index_miss].lru_val = WAYS - 1;
+
+                    for(int i = 0; i < WAYS; i++) begin
+                        if ((i != way_index_miss) && cache_tags[com_miss_req.miss_req_set][i].valid &&
+                                cache_tags[com_miss_req.miss_req_set][i].lru_val > old_lru_index_miss) begin
+                                next_cache_tags[com_miss_req.miss_req_set][i].lru_val =
+                                    cache_tags[com_miss_req.miss_req_set][i].lru_val - 'd1;
+                            end
+                    end
+                end
+            end
+        end else begin
+            next_miss_request.valid             = (!hit && !vc_hit && req_valid && cache_ready) ? 1'b1 : 1'b0;
+            next_miss_request.miss_req_address  = request_address;
+            next_miss_request.miss_req_tag      = d_request_tag;
+            next_miss_request.miss_req_set      = d_request_set;
+            next_miss_request.miss_req_offset   = d_request_offset;
+            next_miss_request.req_is_load       = req_is_load;
+            next_miss_request.miss_req_size     = d_request_size;
+            next_miss_request.miss_req_unsigned = d_req_unsigned;
+            next_miss_request.miss_req_data     = !req_is_load ? req_store_data : '0; 
+            for (int i = 0; i < WAYS; i++) begin
+                if (cache_ready && req_valid && cache_tags[d_request_set][i].valid &&
+                    cache_tags[d_request_set][i].tag == d_request_tag && !hit) begin
+                    data_re = 1'b1;
+                    hit     = 1'b1;
+                    hit_way = i[$clog2(WAYS)-1:0];
+                    
+                    data_raddr = flat_idx(d_request_set, i[$clog2(WAYS)-1:0]); // request for memDP
+
+                    old_lru_index_hit = cache_tags[d_request_set][i].lru_val;// should be 0 if not valid
+
+                    for (int j = 0; j < WAYS; j++) begin
+                        if (j != i && cache_tags[d_request_set][j].valid &&
+                            cache_tags[d_request_set][j].lru_val > old_lru_index_hit) begin
+                            next_cache_tags[d_request_set][j].lru_val =
+                                cache_tags[d_request_set][j].lru_val - 1'b1;
+                        end
+                    end
+                    next_cache_tags[d_request_set][i].lru_val = WAYS - 'd1;
+                end
+            end
+            if(hit && is_valid_load) begin
+                cache_resp_data.address = request_address;
+                cache_resp_data.valid   = 1'b1;
+                case (d_request_size)
+                    BYTE: begin
+                        cache_resp_data.data = d_req_unsigned ?
+                            {{24{1'b0}}, selected_read_data.byte_level[d_request_offset]} :
+                            {{24{selected_read_data.byte_level[d_request_offset][7]}},
+                            selected_read_data.byte_level[d_request_offset]};
+                    end
+                    HALF: begin
+                        cache_resp_data.data = d_req_unsigned ?
+                            {{16{1'b0}}, selected_read_data.half_level[d_request_offset[2:1]]} :
+                            {{16{selected_read_data.half_level[d_request_offset[2:1]][15]}},
+                            selected_read_data.half_level[d_request_offset[2:1]]};
+                    end
+                    WORD: begin
+                        cache_resp_data.data = selected_read_data.word_level[d_request_offset[2]];
+                    end
+                    default: begin
+                        cache_resp_data.data = '0;
+                    end
+                endcase
+                resp_valid = 1'b1;
+            end
+            if(hit && is_valid_store)begin
+                merged_store_data = selected_read_data;
+                data_we    = 1'b1;
+                data_waddr = flat_idx(d_request_set, hit_way);
+                next_cache_tags[d_request_set][hit_way].dirty = 1'b1;
+                case (d_request_size)
+                    BYTE: begin
+                        merged_store_data.byte_level[d_request_offset] = req_store_data[7:0];
+                    end
+                    HALF: begin
+                        merged_store_data.half_level[d_request_offset[2:1]] = req_store_data[15:0];
+                    end
+                    WORD: begin
+                        merged_store_data.word_level[d_request_offset[2]] = req_store_data;
+                    end
+                    default: begin
+                        merged_store_data = '0;
+                    end
+                endcase
+                data_wdata = merged_store_data;
+            end
+            // Vc Load hit
+            if (vc_hit && req_is_load) begin
+                /* unique case (d_request_size)
+                    BYTE: begin
+                        cache_resp_data = d_req_unsigned ?
+                            {{24{1'b0}}, vc_hit_data.byte_level[d_request_offset]} :
+                            {{24{vc_hit_data.byte_level[d_request_offset][7]}},
+                            vc_hit_data.byte_level[d_request_offset]};
+                    end
+
+                    HALF: begin
+                        cache_resp_data = d_req_unsigned ?
+                            {{16{1'b0}}, vc_hit_data.half_level[d_request_offset[2:1]]} :
+                            {{16{vc_hit_data.half_level[d_request_offset[2:1]][15]}},
+                            vc_hit_data.half_level[d_request_offset[2:1]]};
+                    end
+
+                    WORD: begin
+                        cache_resp_data = vc_hit_data.word_level[d_request_offset[2]];
+                    end
+
+                    default: begin
+                        cache_resp_data = '0;
+                    end
+                endcase
+
+                resp_valid = 1'b1;*/
+            end
+        end   
+    end
+
+    // logic record_miss;
+    // assign record_miss = req_valid && cache_ready && !hit && !vc_hit;
+
+    always_ff @(posedge clock) begin
+        if (reset) begin 
+            cache_tags    <= {default: '0};
+            miss_request  <= '0;
+        end
+        else begin
+            cache_tags <= next_cache_tags;
+            miss_request <= next_miss_request; 
+        end
+    end
+
+
+    victim_cache #(.VC_LINES(4)) vc (
+        .clock                (clock),
+        .reset                (reset),
+
+        // dcache miss lookup
+        .dcache_miss_req_valid(vc_lookup_valid), //valid request and not hit
+        .req_is_load          (req_is_load),     
+        .dcache_miss_req_tag  (d_request_tag),
+        .dcache_miss_req_set  (d_request_set),
+        .vc_hit               (vc_hit),
+        .vc_hit_data          (vc_hit_data),
+        .vc_hit_dirty         (vc_hit_dirty),
+
+        // store update from dcache
+        .vc_store_offset      (d_request_offset),
+        .vc_store_size        (d_request_size),
+        .vc_store_data        (req_store_data),
+        .vc_store_ready       (),
+
+        // dcache eviction into VC
+        .dcache_evicted_valid (dcache_evicted_valid),
+        .dcache_evicted_tag   (dcache_evicted_tag),
+        .dcache_evicted_set   (dcache_evicted_set),
+        .dcache_evicted_data  (dcache_evicted_data),
+        .dcache_evicted_dirty (dcache_evicted_dirty),
+        .dcache_evicted_ready (vc_evicted_ready),
+
+        // write buffer
+        .wb_req               (wb_req),
+        .wb_ready             (wb_ready)
+    );
+
+endmodule
---
+++ verilog/src/mshr.sv
@@ -0,0 +1,178 @@
+`include "sys_defs.svh"
+`include "ISA.svh"
+
+module mshr #(
+    parameter int ENTRIES = 16
+)(
+    input logic             clock,
+    input logic             reset,
+
+    input miss_request_t    dcache_miss_req,
+
+    input  MEM_TAG        mem2proc_transaction_tag,
+    input  MEM_TAG        mem2proc_data_tag,
+    input  MEM_BLOCK      mem2proc_data,
+
+    input
+    
+    output MEM_COMMAND    proc2mem_command,
+    output ADDR           proc2mem_addr,
+    output MEM_SIZE       proc2mem_size,
+    output MEM_BLOCK      proc2mem_data,
+    output completed_mshr_t   com_miss_req,
+    output logic          miss_queue_full
+
+);
+      
+    miss_fifo_entry_t miss_fifo      [ENTRIES-1:0];
+    miss_fifo_entry_t next_miss_fifo [ENTRIES-1:0];
+
+    logic [$clog2(ENTRIES)-1:0] miss_head, miss_tail;
+    logic [$clog2(ENTRIES)-1:0] next_miss_head, next_miss_tail;
+    logic [$clog2(ENTRIES):0]   miss_count, next_miss_count;
+
+    req_state_t req_state, next_req_state;
+    miss_fifo_entry_t active_req, next_active_req;
+
+    outstanding_entry_t outstanding_table      [ENTRIES-1:0];
+    outstanding_entry_t next_outstanding_table [ENTRIES-1:0];
+
+    logic found_free_outstanding;
+    logic [$clog2(ENTRIES)-1:0] free_outstanding_idx;
+
+    logic found_completed;
+    logic [$clog2(ENTRIES)-1:0] completed_idx;
+
+    assign miss_queue_full = (miss_count == ENTRIES);
+
+    always_comb begin: MISS_QUEUE_LOGIC
+        next_miss_head         = miss_head;
+        next_miss_tail         = miss_tail;
+        next_miss_count        = miss_count;
+
+        next_miss_fifo         = miss_fifo;
+        next_req_state         = req_state;
+        next_active_req        = active_req;
+        next_outstanding_table = outstanding_table;
+
+        proc2mem_command = MEM_NONE;
+        proc2mem_addr    = '0;
+        com_miss_req     = '0;
+
+        found_free_outstanding = 1'b0;
+        free_outstanding_idx   = '0;
+        
+        found_completed = 1'b0;
+        completed_idx   = '0;
+        ///////////////////////
+        // Enqueue new miss request
+        ////////////////////////
+        if (dcache_miss_req.valid && !miss_queue_full) begin
+            next_miss_fifo[miss_tail].valid             = 1'b1;
+            next_miss_fifo[miss_tail].miss_req_address  = dcache_miss_req.miss_req_address;
+            next_miss_fifo[miss_tail].miss_req_tag      = dcache_miss_req.miss_req_tag;
+            next_miss_fifo[miss_tail].miss_req_set      = dcache_miss_req.miss_req_set;
+            next_miss_fifo[miss_tail].miss_req_offset   = dcache_miss_req.miss_req_offset;
+            next_miss_fifo[miss_tail].req_is_load       = dcache_miss_req.req_is_load;
+            next_miss_fifo[miss_tail].miss_req_size     = dcache_miss_req.miss_req_size;
+            next_miss_fifo[miss_tail].miss_req_unsigned = dcache_miss_req.miss_req_unsigned;
+            next_miss_fifo[miss_tail].miss_req_data     = dcache_miss_req.miss_req_data;
+
+            next_miss_count = next_miss_count + 1'd1;
+            next_miss_tail  = next_miss_tail + 1'd1;
+        end
+
+        for (int i = 0; i < ENTRIES; i++) begin
+            if (!found_free_outstanding && !outstanding_table[i].valid) begin
+                found_free_outstanding = 1'b1;
+                free_outstanding_idx   = i[$clog2(ENTRIES)-1:0];
+            end
+        end
+
+        case (req_state) 
+            REQ_IDLE: begin
+                if(miss_count != 0) begin
+                    next_active_req = miss_fifo[miss_head];
+                    next_req_state = REQ_WAIT_ACCEPT;
+
+                    next_miss_fifo[miss_head] = '0;
+                    next_miss_head = miss_head + 1'd1;
+                    next_miss_count = miss_count - 1'd1;
+                end
+            end
+            REQ_WAIT_ACCEPT: begin
+                if (found_free_outstanding) begin
+                    proc2mem_command = MEM_LOAD;
+                    proc2mem_addr    = active_req.miss_req_address;        
+                    if(mem2proc_transaction_tag != '0) begin 
+                        next_outstanding_table[free_outstanding_idx].valid             = 1'b1;
+                        next_outstanding_table[free_outstanding_idx].trans_tag         = mem2proc_transaction_tag;
+                        next_outstanding_table[free_outstanding_idx].miss_req_address  = active_req.miss_req_address;
+                        next_outstanding_table[free_outstanding_idx].miss_req_tag      = active_req.miss_req_tag;
+                        next_outstanding_table[free_outstanding_idx].miss_req_set      = active_req.miss_req_set;
+                        next_outstanding_table[free_outstanding_idx].miss_req_offset   = active_req.miss_req_offset;
+                        next_outstanding_table[free_outstanding_idx].req_is_load       = active_req.req_is_load;
+                        next_outstanding_table[free_outstanding_idx].miss_req_size     = active_req.miss_req_size;
+                        next_outstanding_table[free_outstanding_idx].miss_req_unsigned = active_req.miss_req_unsigned;
+                        next_outstanding_table[free_outstanding_idx].miss_req_data     = active_req.miss_req_data;
+
+                        next_active_req = '0;
+                        next_req_state  = REQ_IDLE;
+                    end
+                end
+            end
+        endcase
+
+        if (mem2proc_data_tag != '0) begin
+            for (int i = 0; i < ENTRIES; i++) begin
+                if (!found_completed &&
+                    outstanding_table[i].valid &&
+                    outstanding_table[i].trans_tag == mem2proc_data_tag) begin
+                    found_completed = 1'b1;
+                    completed_idx   = i[$clog2(ENTRIES)-1:0];
+                end
+            end
+        end
+
+        if (found_completed) begin
+            com_miss_req.valid             = 1'b1;
+            com_miss_req.miss_req_address  = outstanding_table[completed_idx].miss_req_address;
+            com_miss_req.miss_req_tag      = outstanding_table[completed_idx].miss_req_tag;
+            com_miss_req.miss_req_set      = outstanding_table[completed_idx].miss_req_set;
+            com_miss_req.miss_req_offset   = outstanding_table[completed_idx].miss_req_offset;
+            com_miss_req.req_is_load       = outstanding_table[completed_idx].req_is_load;
+            com_miss_req.miss_req_size     = outstanding_table[completed_idx].miss_req_size;
+            com_miss_req.miss_req_unsigned = outstanding_table[completed_idx].miss_req_unsigned;
+            com_miss_req.miss_req_data     = outstanding_table[completed_idx].miss_req_data;
+            com_miss_req.refill_data       = mem2proc_data;
+
+            next_outstanding_table[completed_idx] = '0;
+        end
+    end
+
+    always_ff @(posedge clock) begin
+        if (reset) begin
+            miss_head <= '0;
+            miss_tail <= '0;
+            miss_count <= '0;
+            req_state <= REQ_IDLE;
+            active_req <= '0;
+
+            for (int i = 0; i < ENTRIES; i++) begin
+                miss_fifo[i] <= '0;
+                outstanding_table[i] <= '0;
+            end
+        end else begin
+            miss_head <= next_miss_head;
+            miss_tail <= next_miss_tail;
+            miss_count <= next_miss_count;
+            req_state <= next_req_state;
+            active_req <= next_active_req;
+
+            for (int i = 0; i < ENTRIES; i++) begin
+                miss_fifo[i] <= next_miss_fifo[i];
+                outstanding_table[i] <= next_outstanding_table[i];
+            end
+        end
+    end
+endmodule
