28899ea7049f88e89137aed7d898a134f9a435b6
Wei-Tung Lai <weitungl@caen-vnc-mi03.engin.umich.edu>
2026-04-03 13:19:03 -0400

    Update Script

--- compare.sh
+++ compare.sh
@@ -14,21 +14,35 @@ echo_color() {
 echo "Comparing ground truth outputs to new processor"
 cd /home/xinzeliu/eecs472/pfinal_472-w26.group8
 
+> result.txt
 for source_file in programs/*.s programs/*c; do
-    program=$(echo "$source_file" | cut -d '.' -f1 | cut -d '/' -f 2)
-    
-        make $program.out
-        echo "Comparing writeback output for $program"
-        diff correct_out/$program.wb output/$program.wb
-        wb_result=$?
-
-    if [ $wb_result -eq 0 ] 
+    if [ "$source_file" = "programs/crt.s" ]
     then
+        continue
+    fi
+    program=$(echo "$source_file" | cut -d '.' -f1 | cut -d '/' -f 2)
+    echo "Running $program"
+    make $program.out
+    echo "Comparing writeback output for $program"
+    diff correct_out/$program.wb output/$program.wb
+    wb_result=$?
+    echo "Comparing memory output for $program"
+    grep '@@@' output/$program.out > mem.txt
+    grep '@@@' correct_out/$program.out > truth_mem.txt
+    diff mem.txt truth_mem.txt
+    mem_result=$?
+    echo "Printing Passed or Failed"
+    if [ $wb_result -eq 0 ] && [ $mem_result -eq 0 ]
+    then 
         fl_result="$program passed"
         echo_color 2 "$fl_result"
+        echo "$fl_result" >> result.txt
+        echo "" >> result.txt
     else
         fl_result="$program failed"
         echo_color 1 "$fl_result"
+        echo "$fl_result" >> result.txt
+        echo "" >> result.txt
     fi
 done
 
