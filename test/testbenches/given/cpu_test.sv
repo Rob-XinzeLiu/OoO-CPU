acf04a0f72c48d2da74761b917744f338b7054cb
Wei-Tung Lai <weitungl@caen-vnc-mi12.engin.umich.edu>
2026-04-11 20:03:18 -0400

    4.11: icache

--- test/testbenches/given/cpu_test.sv
+++ test/testbenches/given/cpu_test.sv
@@ -27,7 +27,7 @@ import "DPI-C" function string decode_inst(int inst);
 //import "DPI-C" function void close_pipeline_output_file();
 
 
-`define TB_MAX_CYCLES 5000000
+`define TB_MAX_CYCLES 50000000
 
 
 module testbench;
